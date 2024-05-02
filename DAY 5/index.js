c18fccb2a664b80f19f78be0baf7a8ae8945dcb2
UTKARSH AGGARWAL <utkarshaggarwalcse@gmail.com>
2024-05-03 01:11:41 +0530

    Add files via upload

---
+++ DAY 5/index.js
@@ -0,0 +1,39 @@
+console.log("HelloWorld");
+
+var num1 = 10;
+let num2 = "gaurav"; 
+const num3 = 23456 ; // when value is not reqd to be changed 
+num4 = 4535 ; // no longer used 
+
+console.log(num3);
+console.log(num4);
+// java  have 3 variables  static , instance
+
+// var had problems lets see what are they 
+
+if (true)
+{ var a = 10;
+
+console.log(a)};
+
+//var can be used anywhere becasue here in scope isnt clear with var 
+
+if (true)
+{ let a = 10;
+
+console.log(a)};
+
+
+////  chsning string to number 
+let numa = null;
+let chnuma = boolean (numa);
+console.log(typeof numa);
+console.log(typeof chnuma);
+console.log(chnuma);
+
+let numa = true;
+let chnuma = string (numa);
+console.log(typeof numa);
+console.log(typeof chnuma);
+console.log(chnuma);
+
