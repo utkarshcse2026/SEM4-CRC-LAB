8185221c0062f53fb116b69d28d43611c576ee81
UTKARSH AGGARWAL <utkarshaggarwalcse@gmail.com>
2024-05-03 01:12:15 +0530

    Add files via upload

---
+++ DAY 6/index.js
@@ -0,0 +1,25 @@
+// in let we can have block scope but with var it doesnt work 
+// reassignment / redeclare can be done in var so its not feasible to use orignal value in that case
+
+//
+
+
+// loosely typed language hai js
+// datatype in js 
+// primitive and non primiitves 
+
+// non primitives :::
+// objects , methods , 
+
+//primitives
+// int , boolean , string , null , undefined , bigint , symbol 
+
+
+let str1 = "abesit";
+let str2 = "school";
+let str3 = "hai";
+
+
+console.log(str1);
+console.log(typeof str1);
+
---
+++ DAY 6/practice.js
@@ -0,0 +1,24 @@
+let name = "llya"
+alert (`hello ${1}`);
+alert (`hello ${"name"}`); // ?
+console.log (`hello ${1}`);
+
+
+
+let a = 1 , b =1 ;
+let c =++a; //?
+let d = b++; //?
+
+
+let a =2 ;
+let x = 1 + (a*=2);
+
+let a = 2;
+let x = 1 + (a*=2);
+
+// code to solve expresiio
+
+a = "-9" -5 / -14
+console.log(a)
+
+
