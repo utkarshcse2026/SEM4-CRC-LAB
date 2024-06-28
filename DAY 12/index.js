c4e9aa7891a9430ed9c4223388839a4cf9a8079c
utkarsh Aggarwal <utkarshaggarwalcse.com>
2024-06-28 15:33:38 +0530

    notes updated

--- DAY 12/index.js
+++ DAY 12/index.js
@@ -6,5 +6,14 @@
 // fetch method 
 // syntax
 // let promise = fetch (url,[options])
+// fetch api return s an object named as promise it contains status of task assigned
 
+//on success it gives ouput and on fail it gives code like 504 , etc
+
+// response.json() =  parses the response in the json file 
+
+
+//json = collections of objects all together like many togrther
+
+//on success handle response and handle the data 
 
